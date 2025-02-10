2f0cdc6e80793b156b2a5ea097715e29583830b7
Yohan <yohancaballeroo@gmail.com>
2025-02-10 21:01:09 +0800

    help

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-// Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal
 var img = document.querySelector(".clickable-image");
 var modalImg = document.querySelector(".modal-image");
 var captionText = document.querySelector(".modal-description");
@@ -10,22 +8,19 @@ img.onclick = function() {
     modal.style.display = "block";
 }
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
+
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
 
-//To move the panels//
 const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
